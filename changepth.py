622ff040b28b8713b25c9332352e03eeb63e86a6
czg1225 <25463066@qq.com>
2023-11-21 12:03:52 +0800

    second

--- changepth.py
+++ changepth.py
@@ -1,17 +1,7 @@
 import numpy as np
-import cv2
-from torch.utils.data import DataLoader
-from torchvision import transforms
-from torch.optim import Adam
-from torch.nn import L1Loss
 import torch
-
 from segment_anything import SamPredictor, sam_model_registry
-from statistics import mean
-#from tqdm import tqdm
-from torch.nn.functional import threshold, normalize
-from segment_anything.utils.transforms import ResizeLongestSide
-from tqdm import tqdm
+
 
 seed = 0
 np.random.seed(seed)
@@ -28,15 +18,17 @@ def train_model():
     print("CUDA Device Name: " + str(torch.cuda.get_device_name(device)))
 
 
+
     # student model
-    model_type = 'vit_l'
-    checkpoint = None
-    student_model = sam_model_registry[model_type](checkpoint=checkpoint)
+    # model_type = 'vit_b'
+    # checkpoint = None
+    # student_model = sam_model_registry[model_type](checkpoint=checkpoint)
+    # print(student_model.image_encoder)
 
-    print(student_model.image_encoder)
 
+    state_dict = torch.load('checkpoints/sam_vit_b_0b3195.pth')
 
-    state_dict = torch.load('/home/chenzigeng/SAM/segment-anything/checkpoints/sam_vit_l_0b3195.pth')
+    
 
     
     for i in range(0,24):
@@ -55,27 +47,5 @@ def train_model():
     
     for key, value in state_dict.items():
         print(key,value.shape)
-    
-    #torch.save(state_dict,'/home/chenzigeng/SAM/segment-anything/checkpoints/sam_vit_l_qkv.pth')
-    
 
-    
-    
-   
-
-
-            
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-
-    train_model()
+    torch.save(state_dict,'checkpoints/sam_vit_b_qkv.pth')
